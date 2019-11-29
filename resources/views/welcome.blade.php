97444013ae3dac86e31c6680ffc81b57c21ba6d8
reddytomas <reddy.tomas@gmail.com>
2019-11-29 19:03:52 -0300

    pagina de welcome, master.css y index.css y agregue las imagenes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,245 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Bootstrap CSS -->
 
-            .full-height {
-                height: 100vh;
-            }
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <!-- Fuentes y nuestro stylesheet -->
+    <link href="https://fonts.googleapis.com/css?family=Ceviche+One|Sedgwick+Ave|Sedgwick+Ave+Display&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Playfair+Display|Raleway|Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css">
 
-            .position-ref {
-                position: relative;
-            }
+    <link rel="stylesheet" href="css/index.css">
+    <title>Altas LLantas</title>
+  </head>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+<body>
+  <div class="container-fluid m-0 p-0">
 
-            .content {
-                text-align: center;
-            }
+      <!-- carousel -------------------------------------->
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Altas Llantas
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+      <section class="vh-80 _carousel">
+        <div class="bd-example">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="IMG/adidas.jpg" class="d-block w-100" alt="...">
         </div>
-    </body>
+        <div class="carousel-item">
+          <img src="IMG/reebok.jpg" class="d-block w-100 " alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="IMG/nike.jpg" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+        </div>
+    </section>
+    <hr>
+<!-- PRODUCTOS ---------------------------------------------->
+    <div><h1 class="_tituloPagina ml-1 mr-1  pt-3 bg-light text-dark pl-3 mb-3 mt-0 text-center" id="productos">Nuestras zapatillas</h1></div>
+    <section class="_articles row p-3 mt-10 mr-0 ml-0" id="productos">
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/azules.png" alt="adidas">
+        <h4><strong>Adidas</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/basquet.png" alt="nike">
+        <h4><strong>Nike</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php" >
+        <img class="d-block w-100"src="IMG/fila.png" alt="reebok">
+        <h4><strong>Reebok</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/gris.png" alt="adidas">
+        <h4 class="color:black"><strong>Adidas</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/vans.png" alt="nike">
+        <h4><strong>Nike</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/swish.png" alt="reebok">
+        <h4><strong>Reebok</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/fila.png" alt="adidas">
+        <h4><strong>Adidas</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/vans.png" alt="nike">
+        <h4><strong>Nike</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php" >
+        <img class="d-block w-100"src="IMG/fila.png" alt="reebok">
+        <h4><strong>Reebok</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/swish.png" alt="adidas">
+        <h4 class="color:black"><strong>Adidas</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/basquet.png" alt="nike">
+        <h4><strong>Nike</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/swish.png" alt="reebok">
+        <h4><strong>Reebok</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/vans.png" alt="adidas">
+        <h4><strong>Adidas</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/basquet.png" alt="nike">
+        <h4><strong>Nike</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php" >
+        <img class="d-block w-100"src="IMG/azules.png" alt="reebok">
+        <h4><strong>Reebok</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/gris.png" alt="adidas">
+        <h4 class="color:black"><strong>Adidas</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/swish.png" alt="nike">
+        <h4><strong>Nike</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+      <article class="_producto col-sm-6 col-md-3 col-lg-2">
+      <a href="detallado.php">
+        <img class="d-block w-100"src="IMG/vans.png" alt="reebok">
+        <h4><strong>Reebok</strong></h4>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </a>
+      </article>
+
+    </section >
+
+    <!-- CONTACTO  y maps----------------------------->
+    <section class="_contacto row p-3 mr-0 ml-1" id="contacto">
+      <article class="col-sm-12 col-md-6" id="_contactarprimera">
+        <form>
+          <h5 id="contactanos"class="_contactanosheader font-weight-bold">Contactanos</h5>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Email</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+          </div>
+
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Comentario</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="acá podes dejar tu mensaje..."></textarea>
+          </div>
+          <button type="button" class="btn btn-primary">Enviar</button>
+        </form>
+
+      </article>
+      <article class="col-sm-12 col-md-6 ">
+        <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3286.186626182054!2d-58.445864885054206!3d-34.54882986210239!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb436efe09303%3A0xfb39818e7624ac76!2sDigital%20House!5e0!3m2!1ses-419!2sar!4v1567563876614!5m2!1ses-419!2sar" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+      </article>
+
+    </section>
+    <!-- FOOTER con redes y nada mas -->
+
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </div>
+  </body>
 </html>
+
+@endsection
