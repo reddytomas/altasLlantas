2120c6dded46be5a90a09cda264c09b1566602de
reddytomas <reddy.tomas@gmail.com>
2019-11-29 18:55:38 -0300

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Altas Llantas
                 </div>
 
                 <div class="links">
