cc7d9b6c7d525b007412d302308564a875428512
tomas grimaux <tgrimaux@gmail.com>
2019-12-04 18:21:19 -0500

    base de datos

---
+++ database/migrations/2019_12_04_222758_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('marca');
+            $table->Integer('categoria');
+            $table->Integer('talle');
+            $table->Integer('precio');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
