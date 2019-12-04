731e8ad76d24272c05c4bc192f9ae7722b243373
tomas grimaux <tgrimaux@gmail.com>
2019-12-04 17:22:22 -0500

    register

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,14 +24,13 @@
 
 <body>
   <div class="container-fluid m-0 p-0">
-      <?php require_once 'parciales/header.php'; ?>
+
       <!-- carousel -------------------------------------->
 
       <section class="vh-80 _carousel">
         <div class="bd-example">
     <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
-        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
         <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
       </ol>
@@ -232,13 +231,14 @@
     </section>
     <!-- FOOTER con redes y nada mas -->
 
-    <?php require_once 'parciales/footer.php'; ?>
+    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </div>
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
   </body>
 </html>
 
