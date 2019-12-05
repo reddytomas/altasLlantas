dee3bcee5fd4de471c7ecd2b6cf05b228525b52f
reddytomas <reddy.tomas@gmail.com>
2019-12-05 11:38:28 -0300

    agregue cosas de php pero no se porque no me funcionan todavia.

---
+++ app/Loader.php
@@ -0,0 +1,18 @@
+<?php
+//require_once('helpers.php');
+require_once('Usuario.php');
+//require_once('clases/validador.php');
+//require_once('clases/baseDato.php');
+//require_once('clases/baseJson.php');
+//require_once("clases/armador.php");
+require_once("Consulta.php");
+////require_once("clases/encriptador.php");
+//require_once("clases/basemysql.php");
+//require_once("clases/validarProducto.php");
+require_once("Producto.php");
+//$validarUsuario = new Validador();
+//$json = new BaseJson("usuarios.json");
+//$registro = new armador();
+//$bd = BaseMysql::conexion();
+$consulta = new Consulta();
+//$validar = new ValidarProducto();
