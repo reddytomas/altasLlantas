16a17cd9bf59aa06115369d7d296aa888d1bf651
reddytomas <reddy.tomas@gmail.com>
2019-12-04 21:28:35 -0300

    control

--- resources/views/crud.blade.php
+++ resources/views/crud.blade.php
@@ -1,7 +1,7 @@
 <?php
   //require_once('loader.php');
   //require_once('controladores/funciones.php');
-  //require_once("clases/Consulta.php");
+  require_once("Controllers/ConsultaController.php");
   //Esta condición la cree para controlar si debo listar las películas en función de lo que usuario quiere ver por medio de consultar o simplemente cuando carga la página
   if ($_GET && !empty(trim('busqueda'))){
     $productos = $consulta->buscarProductos($bd,'productos',$_GET['busqueda']);
@@ -60,7 +60,6 @@
         </tbody>
       </table>
     </div>
-<?php require 'parciales/footer.php' ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
