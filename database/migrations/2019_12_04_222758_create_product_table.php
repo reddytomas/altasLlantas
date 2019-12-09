b23d539ecd845271207e5a3a34e3ae47c69f934f
tomas grimaux <tgrimaux@gmail.com>
2019-12-08 20:18:06 -0500

    tabla creada

--- database/migrations/2019_12_04_222758_create_product_table.php
+++ database/migrations/2019_12_04_222758_create_product_table.php
@@ -13,7 +13,7 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('product', function (Blueprint $table) {
             $table->Increments('id');
             $table->string('marca');
             $table->Integer('categoria');
