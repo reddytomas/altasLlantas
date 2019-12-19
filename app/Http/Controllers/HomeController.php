f0f7934c82f183877653502435ddb6874c92d81f
reddytomas <reddy.tomas@gmail.com>
2019-12-19 18:47:54 -0300

    cambié para que muestre más productos en el home nada más...

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,6 +25,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home')->with('productos',Producto::paginate(12));
+        return view('home')->with('productos',Producto::paginate(36));
     }
 }
